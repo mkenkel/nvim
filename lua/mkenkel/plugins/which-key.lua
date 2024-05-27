9bfcf530171e4cf8ff47387765a7d0a68def4fc6
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-05-27 11:58:13 -0400

    Universal commenting hotkey tool

--- lua/mkenkel/plugins/which-key.lua
+++ lua/mkenkel/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy", -- Lazy.NVIM knows with this that it can load later as it's not important for the UI.
   init = function () -- Runs when Neovim starts up vs when the plugin loads 
     vim.o.timeout = true
-    vim.o.timeoutlen = 300
+    vim.o.timeoutlen = 100
   end,
   opts = {
     -- Loads default config if nothing is present.
