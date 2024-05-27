bdd1956fb407f7d5a042e6e864fe41c00a1f8425
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-05-26 22:44:54 -0400

    More plugins - treesitter, lualine, indent-blankline

---
+++ lua/mkenkel/plugins/dressing.lua
@@ -0,0 +1,5 @@
+return {
+  'stevearc/dressing.nvim',
+  event = "VeryLazy", -- Lazy.NVIM knows with this that it can load later as it's not important for the UI.
+  opts = {},
+}
---
+++ lua/mkenkel/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+  },
+
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { 
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "elixir",
+        "heex",
+        "javascript",
+        "html",
+        -- My extras are going below here
+        "bash",
+        "yaml",
+        "dockerfile",
+        "hcl",
+      },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true }, -- Links up the autotag function to run
+    })
+  end
+}
