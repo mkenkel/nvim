d8817e0544b7057efaf562aa3db8d63d9f7d8b4a
Matthew Kenkel <mattsnoopy2@gmail.com>
2024-06-04 08:50:02 -0400

    Threw gitblames into lualine

--- lua/mkenkel/plugins/lualine.lua
+++ lua/mkenkel/plugins/lualine.lua
@@ -1,10 +1,15 @@
 return {
   'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+    'f-person/git-blame.nvim',
+  },
 
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+    local git_blame = require('gitblame')
+    vim.g.gitblame_display_virtual_text = 0
     require('lualine').setup {
       options = {
         icons_enabled = true,
@@ -27,7 +32,7 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = { { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
         lualine_x = {
           {
             lazy_status.updates,
